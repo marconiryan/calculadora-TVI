fd38eeba38f5482df6e0ebaec0fcd3d18e24bd99
Ryan <86175804+marconiryan@users.noreply.github.com>
2021-12-17 02:01:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 from math import sin
-import numpy
-import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as graph
 
 
-def conta(P1: float, P2: float) -> float:
+def conta(P1: float, P2: float):
     if equacao(P1) * equacao(P2) < 0:
         menor, maior = P1, P2
-        plt.plot(P1, equacao(P1), "o", P2, equacao(P2), "o")
         if equacao(P1) > 0:
             menor, maior = P2, P1
-        while equacao(maior) > 0.1:
+        while round(equacao(maior), 5) != 0.0:
             media = (maior + menor) / 2
             if equacao(media) < 0:
                 menor = media
             else:
                 maior = media
-        print(maior)
         return maior
 
 
-equacao = lambda x: x ** 3 + 5 * sin(x) + 2 * x + 4
+def grafico(P1: float, P2: float) -> None:
+    x = np.linspace(P1, P2, 30)
+    y = list(map(equacao, x))
+    graph.stem(x, y, linefmt='cyan', markerfmt='D')
+    graph.plot(x, y, color="g")
+    graph.show()
 
-Ponto1 = float(input("Ponto 1: "))
-Ponto2 = float(input("Ponto 2: "))
-X = numpy.arange(Ponto1, Ponto2 + 1)
-Y = list(map(equacao, X))
-plt.plot(X, Y)
-plt.plot(conta(Ponto1, Ponto2), 0, 'o')
-plt.show()
+
+if __name__ == "__main__":
+    equacao = lambda x: x ** 3 + 5 * sin(x) + 2 * x + 4
+    Ponto1 = float(input("Ponto 1: "))
+    Ponto2 = float(input("Ponto 2: "))
+    grafico(Ponto1, Ponto2)
