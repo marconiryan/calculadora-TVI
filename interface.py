377963053d8846ab40065e89912497c2a71d0488
Ryan <86175804+marconiryan@users.noreply.github.com>
2021-12-19 05:25:14 -0300

    Provisorio

--- interface.py
+++ interface.py
@@ -6,27 +6,32 @@ class Calculadora:
     def __init__(self, root):
         self.expressao = ""
 
-        self.display = Label(root, font=("Arial", 15))
-        Label(root).grid(column=0,row=0,pady=50)
-        Button(text="1", command=self.BtN1).grid(row=4, column=0, ipadx=15, ipady=15, pady=2, padx=2)
-        Button(text="2", command=self.BtN2).grid(row=4, column=1, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="3", command=self.BtN3).grid(row=4, column=2, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="4", command=self.BtN4).grid(row=3, column=0, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="5", command=self.BtN5).grid(row=3, column=1, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="6", command=self.BtN6).grid(row=3, column=2, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="7", command=self.BtN7).grid(row=2, column=0, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="8", command=self.BtN8).grid(row=2, column=1, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="9", command=self.BtN9).grid(row=2, column=2, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="0", command=self.BtN0).grid(row=5, column=0, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text=",", command=self.BtN0).grid(row=5, column=1, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="=", command=self.BtEqual).grid(row=5, column=3, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="+", command=self.BtSum).grid(row=5, column=2, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="-", command=self.BtSub).grid(row=4, column=3, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="*", command=self.BtMult).grid(row=3, column=3, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="/", command=self.BtDiv).grid(row=2, column=3, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="^", command=self.BtDiv).grid(row=1, column=3, ipadx=5, ipady=5, pady=2, padx=2)
-        Button(text="⌫", command=self.BtBackSpace).grid(row=1, column=0, ipadx=5, ipady=5)
-        self.display.place(height=50,width=50,x=0,y=0)
+        self.display = Label(root, font=("Arial", 15), bg="#696969")
+        Button(text="⌫", command=self.BtBackSpace).place(rely=0.45, relx=0.45, width=120, height=30, x=15, y=-30,anchor="center")
+
+        Button(text="7", command=self.BtN7).place(rely=0.45, relx=0.45, width=30, height=30, x=-30, y=0,anchor="center")
+        Button(text="8", command=self.BtN8).place(rely=0.45, relx=0.45, width=30, height=30, x=0, y=0,anchor="center")
+        Button(text="9", command=self.BtN9).place(rely=0.45, relx=0.45, width=30, height=30, x=30, y=0,anchor="center")
+        Button(text="/", command=self.BtDiv).place(rely=0.45, relx=0.45, width=30, height=30, x=60, y=0,anchor="center")
+
+        Button(text="4", command=self.BtN4).place(rely=0.45, relx=0.45, width=30, height=30, x=-30, y=30,anchor="center")
+        Button(text="5", command=self.BtN5).place(rely=0.45, relx=0.45, width=30, height=30, x=0, y=30,anchor="center")
+        Button(text="6", command=self.BtN6).place(rely=0.45, relx=0.45, width=30, height=30, x=30, y=30,anchor="center")
+        Button(text="*", command=self.BtMult).place(rely=0.45, relx=0.45, width=30, height=30, x=60, y=30,anchor="center")
+
+        Button(text="1", command=self.BtN1).place(rely=0.45, relx=0.45, width=30, height=30, x=-30, y=60,anchor="center")
+        Button(text="2", command=self.BtN2).place(rely=0.45, relx=0.45, width=30, height=30, x=0, y=60,anchor="center")
+        Button(text="3", command=self.BtN3).place(rely=0.45, relx=0.45, width=30, height=30, x=30, y=60,anchor="center")
+        Button(text="-", command=self.BtSub).place(rely=0.45, relx=0.45, width=30, height=30, x=60, y=60,anchor="center")
+
+        Button(text="0", command=self.BtN0).place(rely=0.45, relx=0.45, width=30, height=30, x=-30, y=90,anchor="center")
+        Button(text=",", command=self.BtComma).place(rely=0.45, relx=0.45, width=30, height=30, x=0, y=90,anchor="center")
+        Button(text="=", command=self.BtEqual).place(rely=0.45, relx=0.45, width=30, height=30, x=30, y=90,anchor="center")
+        Button(text="+", command=self.BtSum).place(rely=0.45, relx=0.45, width=30, height=30, x=60, y=90,anchor="center")
+
+        Button(text="^", command=self.BtDiv)
+
+        self.display.pack(side="top", fill=X, pady=5, padx=5)
 
     def update_display(self):
         self.display.config(text=self.expressao)
@@ -85,6 +90,10 @@ class Calculadora:
 
     def BtDiv(self):
         self.expressao += "/"
+        self.update_display()
+
+    def BtComma(self):
+        self.expressao += "."
         self.update_display()
 
     def BtBackSpace(self):
